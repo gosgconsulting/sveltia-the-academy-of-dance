a62c0688b84a01a88d9642ddd77e1e89d6a88ae2
gosgconsulting <contact@gosgconsulting.com>
2025-08-22 21:37:25 +0700

    feat: add Astro demo project with Node.js adapter and updated dependencies

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -1,12 +1,12 @@
 import sveltiaCms from "../dist/astro-sveltia-cms";
 import { defineConfig } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://astro-sveltia-cms.vercel.app",
-	integrations: [sveltiaCms()],
-	output: "hybrid",
-	adapter: cloudflare(),
+  site: "https://astro-sveltia-cms.vercel.app",
+  integrations: [sveltiaCms()],
+  output: "server",
+  adapter: node({ mode: "standalone" }),
 });
